2e5c09d16a1c3ede748c345c2bcc11972888f9b3
Alejandro <oscar.gomez@nuvu.cc>
2024-11-15 11:08:46 -0500

    Correccion de pipeline y test en kutll

--- jenkins/config_mini.sh
+++ jenkins/config_mini.sh
@@ -1,20 +1,20 @@
 # Encode certificate-authority file
-CERT_AUTH=$(base64 -w 0 /home/alejandro/.minikube/ca.crt)
+CERT_AUTH=$(base64 -w 0 /home/nuvu/.minikube/ca.crt)
 
 # Encode client-certificate file
-CLIENT_CERT=$(base64 -w 0 /home/alejandro/.minikube/profiles/minikube/client.crt)
+CLIENT_CERT=$(base64 -w 0 /home/nuvu/.minikube/profiles/minikube/client.crt)
 
 # Encode client-key file
-CLIENT_KEY=$(base64 -w 0 /home/alejandro/.minikube/profiles/minikube/client.key)
+CLIENT_KEY=$(base64 -w 0 /home/nuvu/.minikube/profiles/minikube/client.key)
 
 # Replace certificate-authority
-sed -i "s|certificate-authority: /home/alejandro/.minikube/ca.crt|certificate-authority-data: $CERT_AUTH|" ~/.kube/config
+sed -i "s|certificate-authority: /home/nuvu/.minikube/ca.crt|certificate-authority-data: $CERT_AUTH|" ~/.kube/config
 
 # Replace client-certificate
-sed -i "s|client-certificate: /home/alejandro/.minikube/profiles/minikube/client.crt|client-certificate-data: $CLIENT_CERT|" ~/.kube/config
+sed -i "s|client-certificate: /home/nuvu/.minikube/profiles/minikube/client.crt|client-certificate-data: $CLIENT_CERT|" ~/.kube/config
 
 # Replace client-key
-sed -i "s|client-key: /home/alejandro/.minikube/profiles/minikube/client.key|client-key-data: $CLIENT_KEY|" ~/.kube/config
+sed -i "s|client-key: /home/nuvu/.minikube/profiles/minikube/client.key|client-key-data: $CLIENT_KEY|" ~/.kube/config
 
 cat ~/.kube/config
 
@@ -24,3 +24,14 @@ cat ~/.kube/config
 sudo usermod -a -G docker jenkins
 
 #Y guardar secreto y crear conexion y nube de kubernetes 
+
+
+# Descargar minikube official
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+
+# Instalar krew y despues kuttl
+kubectl krew install kuttl
