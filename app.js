596537a58bef8cba2d5db72f34945d2788ba450e
Alejandro <oscar.gomez@nuvu.cc>
2024-11-22 17:42:21 -0500

    actualizacion a la version 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from jenkins!')
+  res.send('Hello World from jenkins v2!')
 })
 
 app.listen(port, () => {
